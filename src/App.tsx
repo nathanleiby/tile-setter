f7522fd754e2ebcbd28a5b32451105d1c66eeb6c
Nathan Leiby <nathanleiby@gmail.com>
2023-11-10 18:06:02 -0800

    next pass

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,24 @@
+import { AspectRatio, Grid, Slider, Stack } from "@mantine/core";
 import "./App.css";
 
-import * as _ from "lodash";
+import _ from "lodash";
 import { useState } from "react";
 import { Layer, Line, Rect, Stage } from "react-konva";
-import { Button } from "@mantine/core";
 
 // used color picker: https://imagecolorpicker.com/
 // picked from top left lighting
 const COLORS = {
-  FANDANGO: "#e69553",
-  TWIST: "#b8c170",
-  JETSETTER: "#00a6b6",
-  UPTOWN: "#f0f0f0",
-  VOGUE: "#4b5366",
-};
+  WHITE_TILE: "#f0f0f0",
+  BLUE_TILE: "#3e8a94",
+  YELLOW_TILE: "#dcbd7e",
+  ORANGE_TILE: "#bd6021",
+  // VOGUE: "#4b5366",
 
-const colorMapping: { [name: string]: string } = {
-  "orange": COLORS.FANDANGO,
-  "lime": COLORS.TWIST,
-  "cyan": COLORS.JETSETTER,
-  "white": COLORS.UPTOWN,
-  "dark": COLORS.VOGUE,
-}
+  KITCHEN_UPPER_CABINET: "#eeeeee",
+  KITCHEN_LOWER_CABINET: "#a0cccb",
+  KITCHEN_RANGE_ARANCIO: "#f6681d", // ff6e00 // f6681d
+  KITCHEN_RANGE_STAINLESS_STEEL: "#CFD4D9",
+};
 
 interface WallTileProps {
   x: number;
@@ -31,141 +28,184 @@ interface WallTileProps {
 }
 
 const GROUT_COLOR = "lightgray";
-const GROUT_WIDTH = 5;
+const GROUT_WIDTH = 4;
 
 const TILE_WIDTH = 2;
-const TILE_HEIGHT = 2.5;
-const SCALE = 25;
+const SCALE = 50;
 const NUMTILEWIDTH = 40;
 const NUMTILEHEIGHT = 25;
 
-let currentPaintColor = COLORS.FANDANGO;
+const CABINET_HEIGHT = 30;
+
+let lastColor: string;
 
 const WallTile = (props: WallTileProps) => {
+  //      (1,0)
+  // (0,2) (2,2)
+
+  // (0,0) (2,0)
+  //    (1,2)
+
   const baseLocation = props.flipY
     ? [
-        0,
-        SCALE * 1.5,
-        SCALE,
-        SCALE * TILE_HEIGHT,
-        SCALE * TILE_WIDTH,
-        SCALE * 1.5,
-        SCALE,
-        0,
+        (TILE_WIDTH / 2) * SCALE,
+        0 * SCALE,
+        0 * SCALE,
+        TILE_WIDTH * SCALE,
+        TILE_WIDTH * SCALE,
+        TILE_WIDTH * SCALE,
       ]
     : [
-        0,
-        SCALE,
-        SCALE,
-        0,
-        SCALE * TILE_WIDTH,
-        SCALE,
-        SCALE,
-        SCALE * TILE_HEIGHT,
+        0 * SCALE,
+        0 * SCALE,
+        TILE_WIDTH * SCALE,
+        0 * SCALE,
+        (TILE_WIDTH / 2) * SCALE,
+        TILE_WIDTH * SCALE,
       ];
+
   const offsetLocation = baseLocation.map((val, idx) => {
     return idx % 2 === 0 ? val + props.x * SCALE : val + props.y * SCALE;
   });
 
-  // const startColor =
-  //   _.random() > 0.8
-  //     ? COLORS.UPTOWN
-  //     : _.sample([COLORS.FANDANGO, COLORS.VOGUE, COLORS.TWIST]);
-  const [color, setColor] = useState<string>(props.startColor || COLORS.UPTOWN);
-  // .baseLocation.map((x, idx) => (x + ))
-
-  // const randomNewColor = (currentColor: string) => {
-  //   let result;
-  //   while (result === undefined || result === currentColor) {
-  //     result = _.sample(Object.values(COLORS));
-  //   }
-  //   return result;
-  // };
-
   return (
     <Line
       points={offsetLocation}
-      fill={color}
+      fill={props.startColor}
       stroke={GROUT_COLOR}
       strokeWidth={GROUT_WIDTH}
       closed={true}
-      // Konva.Util.getRandomColor()
-      onClick={() => setColor(currentPaintColor)}
     />
   );
 };
 
-const ColorPickerButtons = () => {
-  const availColors = ["orange", "lime", "cyan", "dark", "white"];
-  const [currentColor, setCurrentColor] = useState<string>("orange");
-  const colorButtons:any[] = [];
-  availColors.forEach(function (color) {
-    colorButtons.push(
-    <Button
-      variant={currentColor === color ? "filled" : "outline"} 
-      color={color}
-      onClick={() => {
-        setCurrentColor(color);
-        currentPaintColor = colorMapping[color];
-      }
-      }
-    >
-      {color.toUpperCase()}
-    </Button>);
-  })
-
-  return (
-    <Button.Group>
-      {colorButtons}
-    </Button.Group>
-  )
-}
-
 const App = () => {
-  const simulateLight = false;
   // Stage is a div container
   // Layer is actual canvas element (so you may have several canvases in the stage)
   // And then we have canvas shapes inside the Layer
 
-  const positions: { x: number; y: number; flipY?: boolean; startColor?: string}[] = [];
+  const [pWhite, setPWhite] = useState(0.6);
+
+  const getColor = () => {
+    let color = randomTileColor(pWhite);
+
+    // don't repeat previous color
+    while (color !== COLORS.WHITE_TILE && lastColor === color) {
+      color = randomTileColor(pWhite);
+    }
+    lastColor = color;
+
+    return color;
+  };
+
+  // color biasing -- consider adjacent tiles
+  // TODO
+
+  const positions: {
+    x: number;
+    y: number;
+    flipY?: boolean;
+    color: string;
+  }[] = [];
   for (let xIdx = 0; xIdx < NUMTILEWIDTH; xIdx++) {
     for (let yIdx = 0; yIdx < NUMTILEHEIGHT; yIdx++) {
-      positions.push({ x: xIdx * 2, y: yIdx * 2.5 });
-      positions.push({ x: xIdx * 2 + 1, y: yIdx * 2.5 - 1.5, flipY: true });
+      positions.push({
+        x: xIdx * TILE_WIDTH,
+        y: yIdx * TILE_WIDTH,
+        color: getColor(),
+      });
+      positions.push({
+        x: xIdx * TILE_WIDTH + TILE_WIDTH / 2,
+        y: yIdx * TILE_WIDTH,
+        flipY: true,
+        color: getColor(),
+      });
     }
   }
 
-  const clearGrid = () => {
-
-  }
-
   return (
-    <div>
-      <Stage width={window.innerWidth} height={window.innerHeight}>
-        <Layer>
-          {positions.map((p) => (
-            <WallTile x={p.x} y={p.y} flipY={p.flipY} startColor={p.startColor}/>
-          ))}
-          {/* TODO: figure out how to ignore clicks on the overlay */}
-          {!simulateLight ? null : (
-            <Rect
-              x={0}
-              y={0}
-              width={SCALE * 20}
-              height={SCALE * 50}
-              fillPriority="linear-gradient"
-              fillLinearGradientStartPoint={{ x: 0, y: 0 }}
-              fillLinearGradientEndPoint={{ x: SCALE * 20, y: 0 }}
-              fillLinearGradientColorStops={[0, "#FFF", 1, "#000"]}
-              opacity={0.01}
-            />
-          )}
-        </Layer>
-      </Stage>
-      <ColorPickerButtons />
-      <Button variant="outline" color="white" onClick={()=> clearGrid()}>Clear</Button>
-    </div>
+    <Grid>
+      <Grid.Col span={8}>
+        <AspectRatio ratio={16 / 12}>
+          <Stage width={1600} height={1200}>
+            <Layer>
+              {positions.map((p) => (
+                <WallTile
+                  x={p.x}
+                  y={p.y}
+                  flipY={p.flipY}
+                  startColor={p.color}
+                />
+              ))}
+              <Rect
+                key="upper-cabinet"
+                x={0}
+                y={0}
+                width={SCALE * TILE_WIDTH * NUMTILEWIDTH}
+                height={SCALE * 5}
+                fill={COLORS.KITCHEN_UPPER_CABINET}
+              />
+              <Rect
+                key="lower-cabinet"
+                x={0}
+                y={SCALE * (50 - CABINET_HEIGHT)}
+                width={SCALE * TILE_WIDTH * NUMTILEWIDTH}
+                height={SCALE * CABINET_HEIGHT}
+                fill={COLORS.KITCHEN_LOWER_CABINET}
+              />
+              <Rect
+                key="range-base"
+                x={SCALE * TILE_WIDTH * 5}
+                y={SCALE * (50 - CABINET_HEIGHT)}
+                width={SCALE * TILE_WIDTH * 4}
+                height={SCALE * CABINET_HEIGHT}
+                fill={COLORS.KITCHEN_RANGE_ARANCIO}
+              />
+              <Rect
+                key="range-top"
+                x={SCALE * TILE_WIDTH * 5}
+                y={SCALE * (50 - CABINET_HEIGHT)}
+                width={SCALE * TILE_WIDTH * 4}
+                height={SCALE}
+                fill={COLORS.KITCHEN_RANGE_STAINLESS_STEEL}
+              />
+            </Layer>
+          </Stage>
+        </AspectRatio>
+      </Grid.Col>
+
+      <Grid.Col span={4}>
+        <Stack>
+          Slider:
+          <Slider
+            value={_.round(pWhite, 2)}
+            onChange={setPWhite}
+            min={0}
+            max={1.0}
+            step={0.01}
+          />
+        </Stack>
+      </Grid.Col>
+    </Grid>
   );
 };
 
 export default App;
+
+function randomTileColor(WHITE_VS_NOT: number) {
+  // weighted random
+  let color;
+  if (_.random(0, 1, true) < WHITE_VS_NOT) {
+    color = COLORS.WHITE_TILE;
+  } else {
+    const r = _.random(0, 1, true);
+    if (r < 0.4) {
+      color = COLORS.YELLOW_TILE;
+    } else if (r < 0.8) {
+      color = COLORS.ORANGE_TILE;
+    } else {
+      color = COLORS.BLUE_TILE;
+    }
+  }
+  return color;
+}
